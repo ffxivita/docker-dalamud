62fec3ab84f0b14303cca0b7c346f3010bd37545
DarkArtek <darkartek@gmail.com>
2022-09-20 11:38:28 +0200

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ extensions = ['sphinx_ahd_theme']
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'it'
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
